69cf64ee5f373891d14d9f92eedd8a4967a21821
max3d <vadim@unikivi.ee>
2019-06-22 23:51:23 +0300

    small refactoring

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,27 +1,27 @@
-export const UPDATE_TOTAL = (total) => {
+export const UPDATE_TOTAL = total => {
     return {
         type: UPDATE_TOTAL,
-        payload: total
+        total
     };
 };
 
-export const UPDATE_FROM_CURRENCY = (fromCurr) => {
+export const UPDATE_FROM_CURRENCY = fromCurr => {
     return {
         type: UPDATE_FROM_CURRENCY,
-        payload: fromCurr
-    }
-};
-
-export const UPDATE_TO_CURRENCY = (toCurr) => {
-    return {
-        type: UPDATE_TO_CURRENCY,
-        payload: toCurr
+        fromCurr
     };
 };
 
-export const UPDATE_FROM_AMOUNT = (amount) => {
+export const UPDATE_TO_CURRENCY = toCurr => {
+    return {
+        type: UPDATE_TO_CURRENCY,
+        toCurr
+    };
+};
+
+export const UPDATE_FROM_AMOUNT = amount => {
     return {
         type: UPDATE_FROM_AMOUNT,
-        payload: amount
+        amount
     };
 };
