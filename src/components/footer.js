6e108bacde7600ec3a793fa04a282126663e6b5e
max3d <vadim@unikivi.ee>
2019-06-22 22:57:14 +0300

    footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 const Footer = ({children}) => {
-    return <div className="footer-text">{children}</div>
-
+    return <div className="row fixed-bottom">
+        <div className="footer-text col col-md">{children}</div>
+    </div>
 };
 
 export default Footer;
