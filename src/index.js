7120693ec0cda3bfe157edfe5ef804853ce5f025
max3d <vadim@unikivi.ee>
2019-06-24 00:20:10 +0300

    Provider, store and reducer added to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {reducer} from "./redux/reducers.js";
 import SimpleConverter from "./components/simple-converter.js";
 
+
 const el = document.getElementById("root");
-ReactDOM.render(<SimpleConverter/>, el);
+ReactDOM.render(<Provider store={createStore(reducer)}>
+    <SimpleConverter/>
+</Provider>, el);
