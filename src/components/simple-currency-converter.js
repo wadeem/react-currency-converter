c79e41e95558babf684de7a05170a86e54678e25
vg <vg.unikivi@gmail.com>
2019-07-03 12:32:00 +0300

    axios used to connect to external http resource for loading the currencies

--- src/components/simple-currency-converter.js
+++ src/components/simple-currency-converter.js
@@ -1,67 +1,79 @@
 import React from "react";
 import {Provider} from "react-redux";
 import {createStore} from "redux";
+import axios from "axios";
 import {reducer} from "../redux/reducers.js";
 import HeaderElement from "./header-element.js";
 import FooterElement from "./footer-element.js";
-import {footerText} from "./constants.js";
+import {url, footerText} from "./constants.js";
 import {
     Button, Col, Container, Content, Form, Grid, Input, Item, Row, Text
     , Picker
 } from "native-base";
 
-const SimpleCurrencyConverter = () => {
+class SimpleCurrencyConverter extends React.Component {
 
-    const {
-        topRow, fromCol, toCol, amountCol,
-        buttonCol, resultCol, bottomRow, currenciesRow
-    } = styles;
+    componentDidMount() {
+        console.log("component did mount!");
+        axios.get(url).then(result => {
+            const list = Object.keys(result.data.rates);
+            const currencies = [...list, result.data.base];
+            console.log(list, currencies);
+        }).catch(e => console.error(e))
+    }
 
-    return <Provider store={createStore(reducer)}><Container>
-        <HeaderElement>Currency Converter</HeaderElement>
-        <Grid>
-            <Row size={2} style={topRow}></Row>
-            <Row size={0.75} style={currenciesRow}>
-                <Col style={fromCol}>
-                    <Form>
-                        <Picker mode={"dropdown"} placeholder={"FROM"} onValueChange={e => console.log(e)}>
-                            <Picker.Item label="EUR" value="key0"/>
-                            <Picker.Item label="USD" value="key1"/>
-                        </Picker>
-                    </Form>
-                </Col>
-                <Col style={toCol}>
-                    <Form>
-                        <Picker placeholder={"TO"} onValueChange={e => console.log(e)}>
-                            <Picker.Item label={"USD"} value={"key1"}/>
-                            <Picker.Item label={"EUR"} value={"key0"}/>
-                        </Picker>
-                    </Form>
-                </Col>
-            </Row>
-            <Row size={1}>
-                <Col style={amountCol}>
-                    <Form>
-                        <Item fixedLabel>
-                            <Input placeholder={"Amount"} keyboardType='numeric' onChange={e => console.log(e)}/>
-                        </Item>
-                    </Form>
-                </Col>
-                <Col style={buttonCol}>
-                    <Content>
-                        <Button full onPress={e => console.log(e)}>
-                            <Text>Convert!</Text>
-                        </Button>
-                    </Content>
-                </Col>
-            </Row>
-            <Row size={1}>
-                <Col style={resultCol}><Text>Result: </Text></Col>
-            </Row>
-            <Row size={2} style={bottomRow}></Row>
-        </Grid>
-        <FooterElement>{footerText}</FooterElement>
-    </Container></Provider>
+    render() {
+        const {
+            topRow, fromCol, toCol, amountCol,
+            buttonCol, resultCol, bottomRow, currenciesRow
+        } = styles;
+
+        return <Provider store={createStore(reducer)}><Container>
+            <HeaderElement>Currency Converter</HeaderElement>
+            <Grid>
+                <Row size={2} style={topRow}></Row>
+                <Row size={0.75} style={currenciesRow}>
+                    <Col style={fromCol}>
+                        <Form>
+                            <Picker mode={"dropdown"} placeholder={"FROM"} onValueChange={e => console.log(e)}>
+                                <Picker.Item label="EUR" value="key0"/>
+                                <Picker.Item label="USD" value="key1"/>
+                            </Picker>
+                        </Form>
+                    </Col>
+                    <Col style={toCol}>
+                        <Form>
+                            <Picker placeholder={"TO"} onValueChange={e => console.log(e)}>
+                                <Picker.Item label={"USD"} value={"key1"}/>
+                                <Picker.Item label={"EUR"} value={"key0"}/>
+                            </Picker>
+                        </Form>
+                    </Col>
+                </Row>
+                <Row size={1}>
+                    <Col style={amountCol}>
+                        <Form>
+                            <Item fixedLabel>
+                                <Input placeholder={"Amount"} keyboardType='numeric' onChange={e => console.log(e)}/>
+                            </Item>
+                        </Form>
+                    </Col>
+                    <Col style={buttonCol}>
+                        <Content>
+                            <Button full onPress={e => console.log(e)}>
+                                <Text>Convert!</Text>
+                            </Button>
+                        </Content>
+                    </Col>
+                </Row>
+                <Row size={1}>
+                    <Col style={resultCol}><Text>Result: </Text></Col>
+                </Row>
+                <Row size={2} style={bottomRow}></Row>
+            </Grid>
+            <FooterElement>{footerText}</FooterElement>
+        </Container></Provider>
+    }
 };
 
 const styles = {
